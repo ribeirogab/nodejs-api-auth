1097aa3427d6df2b86057da36a882b4a309a110c
ribeirogab <ribeirogabx@gmail.com>
2024-09-13 15:04:38 -0300

    chore: update lambda API Gateway URL in outputs.tf file

--- infra/lambda/outputs.tf
+++ infra/lambda/outputs.tf
@@ -1,4 +1,4 @@
 output "api_gateway_url" {
   description = "The invoke URL of the API Gateway"
-  value       = "https://${aws_api_gateway_rest_api.nodejs_api_authentication_apigateway.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.api_deployment.stage_name}/test"
+  value       = "https://${aws_api_gateway_rest_api.nodejs_api_authentication_apigateway.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.api_deployment.stage_name}/health"
 }
