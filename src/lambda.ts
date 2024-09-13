dfa627d740d96f4cf89e0762300b18115df74293
ribeirogab <ribeirogabx@gmail.com>
2024-09-13 16:18:16 -0300

    fix: remove unnecessary async function definition

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,17 +1,5 @@
-import { type PromiseHandler, awsLambdaFastify } from '@fastify/aws-lambda';
+import { awsLambdaFastify } from '@fastify/aws-lambda';
 
 import { main } from './main';
 
-let app: PromiseHandler;
-
-const proxy = async () => {
-  if (app) {
-    return app;
-  }
-
-  app = awsLambdaFastify(main().app);
-
-  return app;
-};
-
-export const handler = proxy;
+export const handler = awsLambdaFastify(main().app);
