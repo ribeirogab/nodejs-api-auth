68f79712b644308e6fc7d04c12d77ce3a20550d5
ribeirogab <ribeirogabx@gmail.com>
2024-09-20 17:10:06 -0300

    fix: fix incorrect hash and range key in dynamodb table configuration

--- infra/lambda/main.tf
+++ infra/lambda/main.tf
@@ -6,17 +6,22 @@ resource "aws_dynamodb_table" "auth_resource_table" {
   name         = "${var.environment}-${var.dynamodb_table_name}"
   billing_mode = "PAY_PER_REQUEST"
 
-  hash_key  = "SK"
-  range_key = "PK"
+  hash_key  = "PK"
+  range_key = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
 
   attribute {
     name = "SK"
     type = "S"
   }
 
-  attribute {
-    name = "PK"
-    type = "S"
+  ttl {
+    attribute_name = "TTL"
+    enabled        = true
   }
 
   tags = {
